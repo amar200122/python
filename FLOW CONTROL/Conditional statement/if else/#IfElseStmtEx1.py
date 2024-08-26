b112cb2c01f595d7ed60fde756cbb40991baae34
amar200122 <amarakhil.p@gmail.com>
2024-08-26 15:50:19 +0530

    Added notes and project files from PythonProjects

---
+++ FLOW CONTROL/Conditional statement/if else/#IfElseStmtEx1.py
@@ -0,0 +1,12 @@
+#Program finding Biggest of Two Number and check for equality by using if..else stmt
+#IfElseStmtEx1.py
+a=float(input("Enter First Value:")) # 10
+b=float(input("Enter Second Value:")) # 2
+if(a>b):
+    print("big({},{})={}".format(a,b,a))
+else:
+    if(b>a):
+        print("big({},{})={}".format(a, b, b))
+    else:
+        print("Both Values equal")
+print("Program execution completed")
---
+++ MODULES/programs for modules/#SE2.py
@@ -0,0 +1,5 @@
+#SE2.py
+import Aop
+Aop.sumop(10,20) # function call
+Aop.subop(5,6) # Function call
+Aop.mulop(10,2) # function call
